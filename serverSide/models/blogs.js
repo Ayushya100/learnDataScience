d7758a5bd66a147e26b9ad6a3fccf94e0e69d053
Ayushya <jaiswalayushya@gmail.com>
2022-07-10 19:53:10 +0530

    Server Side Initialization

---
+++ serverSide/models/blogs.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+// Collection Schema
+const blogsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    authorImg: {
+        type: String
+    },
+    img: {
+        type: String
+    },
+    version: {
+        type: String,
+        default: "v1.0"
+    },
+    overview: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    tags: {
+        type: Array,
+        required: true
+    }
+});
+
+// Create Collection
+const Blogs = new mongoose.model("Blog", blogsSchema);
+
+module.exports = Blogs;
